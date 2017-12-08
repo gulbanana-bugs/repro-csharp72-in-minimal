324ee48ef71c3213a8d3cc785f515f4f007671c3
Thomas Castiglione <thomas@agcom.com.au>
2017-12-08 21:07:20 +0800

    create repro

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main() => new[]{ "" }.Select(InMethod).ToList();
+    static string InMethod(in string param) => param;
+}
